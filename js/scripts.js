630e38a8078ee88a4ad71eee02f39395a824af4c
Misan Teodora <teodora.misan@yahoo.com>
2022-05-03 23:05:52 +0300

    Second iteration

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,38 @@
 // DOM selector for autocomplete
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
+  var elems = document.querySelectorAll('.autocomplete');
 
-    var instances = M.Autocomplete.init(elems,
-      {    data: {
-              "Aruba": null,
-              "Cancun Mexico": null,
-              "Hawaii": null,
-              "Florida": null,
-              "California": null,
-              "Jamaica": null,
-              "Europe": null,
-              "The Bahamas": null,
-            }});
+  var instances = M.Autocomplete.init(elems, {
+    data: {
+      "Romania": null,
+      "Italy": null,
+      "The Netherlands": null,
+      "Spain": null,
+      "California": null,
+      "France": null,
+      "Lithuania": null,
+      "The Bahamas": null,
+    }
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sideNav = document.querySelector('.sidenav');
+  var instace = M.Sidenav.init(sideNav, {});
+
+});
+
+
+// Scrollspy
+
+document.addEventListener('DOMContentLoaded', function() {
+  const ss = document.querySelectorAll('.scrollspy');
+  var instace = M.ScrollSpy.init(ss, {});
+});
+
+
+// Material Boxed
+document.addEventListener('DOMContentLoaded', function() {
+  const mb = document.querySelectorAll('.materialboxed');
+  var instace = M.Materialbox.init(mb, {});
+});
